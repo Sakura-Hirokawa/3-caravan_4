eeeb7cf268d5949e54864ef1c9c51a02e849da56
Sakura-Hirokawa <sk.hirokawa26@gmail.com>
2021-04-22 21:40:25 +0000

    [Add]新規投稿、保存

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,12 @@
 class BlogsController < ApplicationController
   def new
+    @blog=Blog.new
+  end
+
+  def create
+    blog=Blog.new(blog_params)
+    blog.save
+    redirect_to blogs_path
   end
 
   def index
@@ -9,5 +16,11 @@ class BlogsController < ApplicationController
   end
 
   def edit
+  end
+
+  private
+
+  def blog_params
+    params.require(:blog).permit(:title, :category, :body)
   end
 end
