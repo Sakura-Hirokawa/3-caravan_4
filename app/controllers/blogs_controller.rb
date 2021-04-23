ba3df8d22c08e0aa10c6d4fded4e3f942881305c
Sakura-Hirokawa <sk.hirokawa26@gmail.com>
2021-04-23 00:16:52 +0000

    [Add]refile追加

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -36,6 +36,6 @@ class BlogsController < ApplicationController
   private
 
   def blog_params
-    params.require(:blog).permit(:title, :category, :body)
+    params.require(:blog).permit(:title, :category, :body, :image)
   end
 end
