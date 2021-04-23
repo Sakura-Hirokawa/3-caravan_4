7355ddfc29d0666665d1c3010bf1d4bf6b2b1b9e
Sakura-Hirokawa <sk.hirokawa26@gmail.com>
2021-04-23 01:14:27 +0000

    [Add]page

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'd292dd4c0dcd06c4934bf245c31077be27d40f3d28f2121e403301c82f19bc793aab1efbd8f772f94325a04af3db88d2354997e421492714ab5d95943f90175d'
